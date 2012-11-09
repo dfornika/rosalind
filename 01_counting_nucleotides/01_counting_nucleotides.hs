9e1740e74a14fb6d81c43d022f65f2470d0eb878
Dan Fornika <dfornika@gmail.com>
2012-11-08 21:00:17 -0800

    first commit

---
+++ 01_counting_nucleotides/01_counting_nucleotides.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import IO
+import Control.Monad
+import Data.Map (fromListWith, elems)
+import Data.List (intersperse)
+
+main :: IO()
+main = do
+  seq <- getLine
+  let baseMap = fromListWith (+) [(c, (read "1" :: Int)) | c <- seq]
+  let output = elems baseMap
+  putStrLn $ (show $ output!!0) ++ " " ++ (show $ output!!1) ++ " " ++ (show $ output!!2) ++ " " ++ (show $ output!!3) 
