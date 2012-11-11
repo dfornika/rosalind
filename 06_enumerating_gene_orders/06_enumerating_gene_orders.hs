3e7ceabcc529077461dffce43b8243cb1cc296b6
Dan Fornika <dfornika@gmail.com>
2012-11-11 13:10:26 -0800

    completed problem 06

--- 06_enumerating_gene_orders/06_enumerating_gene_orders.hs
+++ 06_enumerating_gene_orders/06_enumerating_gene_orders.hs
@@ -1,7 +1,13 @@
 module Main where
 
+import Data.List (permutations, intersperse)
+
 main :: IO()
 main = do
   input <- getLine
 
-    
+  putStrLn $ show $ length $ permutations [1..(read input :: Int)]
+  mapM_ putStrLn $ map ints2string $ permutations [1..(read input :: Int)]
+
+ints2string :: [Int] -> String
+ints2string xs = intersperse ' ' (concat $ map show xs) 
