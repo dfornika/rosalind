3e116c60d9455f2b9407152b4872242ab3ad7ae0
Dan Fornika <dfornika@gmail.com>
2012-11-11 13:56:47 -0800

    completed problem 07

--- 07_intro_to_probability/07_intro_to_probability.hs
+++ 07_intro_to_probability/07_intro_to_probability.hs
@@ -1,7 +1,13 @@
 module Main where
 
-import Data.List (permutations, intersperse)
+import Text.Printf (printf)
 
 main :: IO()
 main = do
   input <- getLine
+  let gcRatios = map (\x -> read x :: Float) (words input)
+
+  mapM_ (printf "%.3f ") $ map probIdentical gcRatios 
+
+probIdentical :: Float -> Float
+probIdentical x = (2*((1 - x)/2)^2) + (2*(x/2)^2)
