0ff21bb2cf228ac7bfd852a5cf5b6cc7433fa003
Dan Fornika <dfornika@gmail.com>
2012-11-11 21:33:11 -0800

    implemented oneProfile function

--- 10_consensus_and_profile/10_consensus_and_profile.hs
+++ 10_consensus_and_profile/10_consensus_and_profile.hs
@@ -8,7 +8,27 @@ main = do
   let seqs = lines input
   
   mapM_ putStrLn seqs
-  print $ countOccurences (seqs!!0) 'A'
+  putStrLn $ show $ countOccurences (seqs!!0) 'A'
+  
+  putStrLn $ map head seqs  
+  
+  putStrLn $ show $ oneProfile seqs
 
 countOccurences :: String -> Char -> Int
 countOccurences seq base = length $ findIndices (\x -> x == base) seq
+
+oneProfile :: [String] -> [Int]
+oneProfile [] = []
+oneProfile seqs = countOccurences currentColumn 'A' 
+                : countOccurences currentColumn 'C'
+                : countOccurences currentColumn 'G'
+                : countOccurences currentColumn 'T'
+                : []
+    where currentColumn = map head seqs
+
+int2base :: Int -> Char
+int2base 0 = 'A'
+int2base 1 = 'C'
+int2base 2 = 'G'
+int2base 3 = 'T'
+int2base _ = 'N'
