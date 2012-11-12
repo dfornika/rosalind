d1063bf07ec1647a649412d94ae3f08222d897b1
Dan Fornika <dfornika@gmail.com>
2012-11-12 09:36:24 -0800

    completed solution to problem 10

--- 10_consensus_and_profile/10_consensus_and_profile.hs
+++ 10_consensus_and_profile/10_consensus_and_profile.hs
@@ -8,12 +8,15 @@ main = do
   input <- getContents
 
   let seqs = lines input
+
+  -- posList is a list of positions, the same size as our input seqs
+  -- Helpful for working with 1-indexed coordinates
   let posList = [1..(length $ seqs!!0)]
 
-  mapM_ putStrLn seqs 
-  
   let profileMatrix =  map (oneProfile seqs) posList
-  
+
+  putStrLn $ map head (map oneConsensus profileMatrix)
+
   printf "%s" "A: "
   mapM_ (printf "%d ") (map (!!0) profileMatrix)
   printf "%s" "\n"
@@ -33,12 +36,13 @@ main = do
 countOccurences :: String -> Char -> Int
 countOccurences seq base = length $ findIndices (\x -> x == base) seq
 
-{-- Take a list of DNA sequences, plus a position and return the 
+{-- oneProfile:
+    Take a list of DNA sequences, plus a position (1-indexed) and return the 
     profile for that position in the order [A, C, G, T]
 
     e.g. ["AATCT"
          ,"ACTTT"  -> 2 -> [2,1,0,0]
-         ,"TAGGT"]
+         ,"TAGGT"] 
 --}
 oneProfile :: [String] -> Int -> [Int]
 oneProfile [] _ = []
@@ -48,6 +52,17 @@ oneProfile seqs pos = countOccurences currentColumn 'A'
                     : countOccurences currentColumn 'T'
                     : []
     where currentColumn = map (!!(pos-1)) seqs
+
+{-- oneConsensus:
+    Given one 'slice' of the profileMatrix representing one sequence position,
+    return the character (A,C,G,T) representing the consensus base.
+
+    If there is a tie, return a list of all consensus bases.
+--}
+oneConsensus :: [Int] -> [Char]
+oneConsensus [] = "N"
+oneConsensus xs = map int2base indexOfMaxValue
+    where indexOfMaxValue = findIndices (\x -> x == maximum xs) xs 
 
 -- Quick utility function useful for working with lists 
 -- that are formatted [A, C, G, T]
