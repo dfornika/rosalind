41503851a23727c236063a2db0d75eba0dd5c673
Dan Fornika <dfornika@gmail.com>
2012-11-13 19:37:10 -0800

    problem 12 seems to be solved for small strings, but breaks down for strings larger than ~50 chars

---
+++ 12_pitfalls_reverse_translation/12_pitfalls_reverse_translation.hs
@@ -0,0 +1,36 @@
+module Main where
+
+import qualified Data.Map as Map
+import Control.Monad (liftM)
+import Text.Printf (printf)
+
+main :: IO ()
+main = do
+  seq <- getLine
+  
+  -- oof, this one's ugly
+  putStrLn $ show $ (foldl1 (*) (map aa2int seq)) * 3 `mod` 1000000
+
+aa2int :: Char -> Int
+aa2int aa = Map.findWithDefault 1 aa aaMap
+           where aaMap = Map.fromList [ ('F', 2)
+                                      , ('L', 6)
+                                      , ('I', 3)
+                                      , ('M', 1)
+                                      , ('V', 4)
+                                      , ('S', 4)
+                                      , ('P', 4)
+                                      , ('T', 4)
+                                      , ('A', 4)
+                                      , ('Y', 2)
+                                      , ('H', 2)
+                                      , ('Q', 2)
+                                      , ('N', 2)
+                                      , ('K', 2)
+                                      , ('D', 2)
+                                      , ('E', 2)
+                                      , ('C', 2)
+                                      , ('W', 1)
+                                      , ('R', 6)
+                                      , ('S', 2)
+                                      , ('G', 4) ]
