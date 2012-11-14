2b2e3b6c044425a7a48f0e665eed718a2990e66a
Dan Fornika <dfornika@gmail.com>
2012-11-13 17:48:14 -0800

    working on problem 11.  Runs, but gives incorrect answer for sample data.

--- 11_intro_to_expected_value/11_intro_to_expected_value.hs
+++ 11_intro_to_expected_value/11_intro_to_expected_value.hs
@@ -1,9 +1,24 @@
 module Main where
 
+import Control.Monad (liftM)
+
 main :: IO ()
 main = do
-  ints          <- getLine
-  probabilities <- getLine
-  let m = ints!!0
-  let n = ints!!1
+  ints       <- liftM words getLine
+  gcContents' <- liftM words getLine
+  let m = read $ (ints!!0)::Int
+  let n = read $ (ints!!1)::Int
+  let gcContents = map (\x -> read x :: Float) gcContents'
 
+  mapM_ putStrLn $ map show gcContents
+  mapM_ putStrLn $ map show $ map (expectedValue m n) gcContents
+
+expectedValue :: Int -> Int -> Float -> Float
+expectedValue m n gc = fromIntegral (n - (m - 1)) * probIdentical m gc
+
+probIdentical :: Int -> Float -> Float
+probIdentical m gc = fromIntegral m * probOneBaseIdentical gc
+
+-- Taken from my solution to Intro to Probability (PROB)
+probOneBaseIdentical :: Float -> Float
+probOneBaseIdentical x = (2*((1 - x)/2)^2) + (2*(x/2)^2)
