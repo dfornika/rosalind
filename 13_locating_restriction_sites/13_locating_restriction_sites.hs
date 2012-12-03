261b719d707d0f2121c56faab09cbf7591ebfb94
Dan Fornika <dfornika@gmail.com>
2012-12-03 13:10:21 -0800

    getting closer to exercise 13 solution...

--- 13_locating_restriction_sites/13_locating_restriction_sites.hs
+++ 13_locating_restriction_sites/13_locating_restriction_sites.hs
@@ -1,11 +1,24 @@
+module Main where
+
 main :: IO ()
 main = do
   seq <- getLine
-  putStrLn $ show $ map (findPalindrome seq) [4..12]
+  let len = length seq
+  let restrict_sites' =
+        filter (\x -> fst x /= 0) $
+        concat $ map (findPalindrome seq) [4..12]
+  let restrict_sites = zip (map (len -) (map fst restrict_sites'))
+                           (map snd restrict_sites')
+
+  mapM_ putStrLn $ map printOneSite restrict_sites
+
+printOneSite :: (Int, Int) -> String
+printOneSite (a, b) = (show a) ++ " " ++ (show b)
 
 findPalindrome :: String -> Int -> [(Int, Int)]
 findPalindrome [] _ = []
 findPalindrome seq n
+  | length seq < n = []
   | take n seq == revComp (take n seq) = (length seq, n) : findPalindrome (tail seq) n
   | otherwise                          = (0,0) : findPalindrome (tail seq) n
 
