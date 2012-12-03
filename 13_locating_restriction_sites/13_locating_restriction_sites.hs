ed501471d8f05b253510b6b617304cc6901251d2
Dan Fornika <dfornika@gmail.com>
2012-12-03 13:56:20 -0800

    solved exercise 13

--- 13_locating_restriction_sites/13_locating_restriction_sites.hs
+++ 13_locating_restriction_sites/13_locating_restriction_sites.hs
@@ -1,13 +1,16 @@
 module Main where
 
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
 main :: IO ()
 main = do
   seq <- getLine
   let len = length seq
   let restrict_sites' =
-        filter (\x -> fst x /= 0) $
-        concat $ map (findPalindrome seq) [4..12]
-  let restrict_sites = zip (map (len -) (map fst restrict_sites'))
+        concat $ map (findPalindrome seq len) [4..12]
+  let restrict_sites = sortBy (comparing fst) $
+                       zip (map ((len + 1) -) (map fst restrict_sites'))
                            (map snd restrict_sites')
 
   mapM_ putStrLn $ map printOneSite restrict_sites
@@ -15,12 +18,12 @@ main = do
 printOneSite :: (Int, Int) -> String
 printOneSite (a, b) = (show a) ++ " " ++ (show b)
 
-findPalindrome :: String -> Int -> [(Int, Int)]
-findPalindrome [] _ = []
-findPalindrome seq n
+findPalindrome :: String -> Int -> Int -> [(Int, Int)]
+findPalindrome [] _ _ = []
+findPalindrome seq m n
   | length seq < n = []
-  | take n seq == revComp (take n seq) = (length seq, n) : findPalindrome (tail seq) n
-  | otherwise                          = (0,0) : findPalindrome (tail seq) n
+  | take n seq == revComp (take n seq) = (length seq, n) : findPalindrome (tail seq) m n
+  | otherwise                          = findPalindrome (tail seq) m n
 
 revComp :: String -> String
 revComp seq = reverse $ map complement seq
