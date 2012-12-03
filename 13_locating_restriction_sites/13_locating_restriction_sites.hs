d557868a1fbf87839e9d69a040b3fe941dc00bfa
Dan Fornika <dfornika@gmail.com>
2012-12-02 21:53:44 -0800

    updated exercise 13

--- 13_locating_restriction_sites/13_locating_restriction_sites.hs
+++ 13_locating_restriction_sites/13_locating_restriction_sites.hs
@@ -3,11 +3,11 @@ main = do
   seq <- getLine
   putStrLn $ show $ map (findPalindrome seq) [4..12]
 
-findPalindrome :: String -> Int -> [(Bool, Int)]
-findPalindrome [] n = []
+findPalindrome :: String -> Int -> [(Int, Int)]
+findPalindrome [] _ = []
 findPalindrome seq n
-  | take n seq == revComp (take n seq) = (True, n):findPalindrome (tail seq) n
-  | take n seq /= revComp (take n seq) = (False, n):findPalindrome (tail seq) n
+  | take n seq == revComp (take n seq) = (length seq, n) : findPalindrome (tail seq) n
+  | otherwise                          = (0,0) : findPalindrome (tail seq) n
 
 revComp :: String -> String
 revComp seq = reverse $ map complement seq
