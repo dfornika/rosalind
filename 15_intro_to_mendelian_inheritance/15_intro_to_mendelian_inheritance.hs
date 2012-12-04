53dc5ef7a7f3744557463760f96b36d28c9aa274
Dan Fornika <dfornika@gmail.com>
2012-12-04 10:37:30 -0800

    added probability table comment to clarify evaluation strategy

--- 15_intro_to_mendelian_inheritance/15_intro_to_mendelian_inheritance.hs
+++ 15_intro_to_mendelian_inheritance/15_intro_to_mendelian_inheritance.hs
@@ -5,16 +5,34 @@ import Data.Ratio
 
 main :: IO ()
 main = do
+  -- It's ugly, but this line converts a string of whitespace-separated
+  -- numbers into a list of integers
   input <- liftM (map (\x -> read x :: Int)) $ liftM words getLine
-  putStrLn $ show $ input
---  putStrLn $ show $ probPosessDom (input!!0) (input!!1) (input!!2)
 
-probPosessDom :: Int -> Int -> Int -> Double
-probPosessDom k m n = probHomoDom + probHet
-  where probSelected x = (fromIntegral x) / (fromIntegral (k + m + n))
-        probHomoDom = undefined
-        probHet     = undefined
-        probHomoRec = undefined
+  putStrLn $ show $ probPosessDom (input!!0) (input!!1) (input!!2)
+
+{-- Consider the following probability table:
+
+    firstMate            | A A | A A | A A | A a | A a | A a | a a | a a | a a |
+    secondMate           | A A | A a | a a | A A | A a | a a | A A | A a | a a |
+    probOffspringHomoDom |  1  | 1/2 |  0  | 1/2 | 1/4 |  0  |  0  |  0  |  0  |
+    probOffspringHet     |  0  | 1/2 |  1  | 1/2 | 1/2 | 1/2 |  1  | 1/2 |  1  |
+    probOffspringHomoRec |  0  |  0  |  0  |  0  | 1/4 | 1/2 |  0  | 1/2 |  1  |
+--}
+
+probOffspringPosessDom :: Int -> Int -> Int -> Double
+probOffspringPosessDom k m n = probOffspringHomoDom + probOffspringHet
+  where probOffspringHomoDom = probSecondMateHomoDom (probFirstMateHomoDom k m n) k m n
+                               probSecondMateHomoDom (probFirstMateHet k m n) k m n
+                               probSecondMateHomoDom (probFirstMateHomoRec k m n) k m n
+                               probSecondMateHet (probFirstMateHomoDom k m n) k m n
+                               probSecondMateHet (probFirstMateHet k m n) k m n
+                               probSecondMateHet (probFirstMateHomoRec k m n) k m n
+                               probSecondMateHomoRec (probFirstMateHomoDom k m n) k m n
+                               probSecondMateHomoRec (probFirstMateHet k m n) k m n
+                               probSecondMateHomoRec (probFirstMateHomoRec k m n) k m n
+        probOffspringHet     = undefined
+        probOffspringHomoRec = undefined
 
 probFirstMateHomoDom :: Int -> Int -> Int -> Rational
 probFirstMateHomoDom k m n = (fromIntegral k) % (fromIntegral (k + m + n))
@@ -25,5 +43,5 @@ probFirstMateHet k m n = (fromIntegral m) % (fromIntegral (k + m + n))
 probFirstMateHomoRec :: Int -> Int -> Int -> Rational
 probFirstMateHomoRec k m n = (fromIntegral n) % (fromIntegral (k + m + n))
 
-probSecondMateHomoDom :: Double -> Double -> Double -> Double
-probSecondMateHomoDom = undefined
+probSecondMateHomoDom :: Rational -> Int -> Int-> Int -> Rational
+probSecondMateHomoDom x k m n = undefined
