e6eada9b311974c8eeff11cea47f854cfba30b4c
Dan Fornika <dfornika@gmail.com>
2012-12-03 18:51:17 -0800

    refactored sequences into label-seq pairs in exercise 14

--- 14_overlap_graphs/14_overlap_graphs.hs
+++ 14_overlap_graphs/14_overlap_graphs.hs
@@ -2,19 +2,28 @@ module Main where
 
 import System.Environment (getArgs)
 import qualified Data.ByteString.Lazy.Char8 as B
-import Bio.Core.Sequence (SeqData, unSD, seqdata)
+import Bio.Core.Sequence (SeqData, SeqLabel, unSD, unSL, seqdata, seqlabel)
 import Bio.Sequence.Fasta (readFasta)
 
 main :: IO ()
 main = do
   (file:_) <- getArgs
-  seqs <- readFasta file
-  mapM_ putStrLn $ map seq2string (map seqdata seqs)
+  input <- readFasta file
+  let seqs = map seq2string (map seqdata input)
+  let labels = map label2string (map seqlabel input)
+  let slPairs = zip labels seqs
 
-matchSeqs :: String -> String -> Int -> Bool
-matchSeqs s1 s2 n
+  putStrLn $ show $
+    map (matchSeqs 3 $ seqs!!0) (tail seqs)
+  putStrLn $ show slPairs
+
+matchSeqs :: Int -> String -> String -> Bool
+matchSeqs n s1 s2
   | (reverse $ take n (reverse s1)) == take n s2 = True
-  | otherwise                                  = False
+  | otherwise                                    = False
 
 seq2string :: SeqData -> String
 seq2string = B.unpack . unSD
+
+label2string :: SeqLabel -> String
+label2string = B.unpack . unSL
