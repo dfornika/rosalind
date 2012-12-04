c513d96301652648c6c2d7604bfe56a857017f50
Dan Fornika <dfornika@gmail.com>
2012-12-03 20:52:10 -0800

    commented a couple of the more obtuse functions and data structures in exercise 14

--- 14_overlap_graphs/14_overlap_graphs.hs
+++ 14_overlap_graphs/14_overlap_graphs.hs
@@ -14,10 +14,22 @@ main = do
   let labels = map label2string (map seqlabel input)
   let slPairs = zip labels seqs
 
-  putStrLn $ show $
-    concat $ map (pairUp slPairs) slPairs
+  {-- 'combinations' is a list of all possible combinations of sequences,
+      excluding sequences paired with themselves. It has the structure:
+           [((String, String), (String, String))]
+      e.g. [(("Rosalind_0442", "AAATCCC"), ("Rosalind_2391", "AAATTTT"))]
+  --}
+  let combinations = concat $ map (pairUp slPairs) slPairs
 
-  putStrLn $ show slPairs
+  {-- the 'outputMap' has keys that are pairs of sequences
+      like ((String, String), (String, String)) and Bool values
+      indicating whether the last three bases of the first sequence
+      match the first three bases of the second sequence
+  --}
+  let outputMap = Map.fromList $
+                  zip (concat $ map (pairUp slPairs) slPairs)
+                      (map (matchSeqs 3) combinations)
+  putStrLn $ show outputMap
 
 matchSeqs :: Int -> ((String, String), (String, String)) -> Bool
 matchSeqs n ((l1, s1), (l2, s2))
