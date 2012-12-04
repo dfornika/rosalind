e4e6331b6ec74fba7bcfd6be24a25ca8b9aa64b0
Dan Fornika <dfornika@gmail.com>
2012-12-03 18:27:46 -0800

    implemented matching function for overlap graph in exercise 14

--- 14_overlap_graphs/14_overlap_graphs.hs
+++ 14_overlap_graphs/14_overlap_graphs.hs
@@ -1,11 +1,20 @@
 module Main where
 
 import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy.Char8 as B
+import Bio.Core.Sequence (SeqData, unSD, seqdata)
 import Bio.Sequence.Fasta (readFasta)
 
 main :: IO ()
 main = do
   (file:_) <- getArgs
   seqs <- readFasta file
+  mapM_ putStrLn $ map seq2string (map seqdata seqs)
 
-  mapM_ putStrLn $ map show seqs
+matchSeqs :: String -> String -> Int -> Bool
+matchSeqs s1 s2 n
+  | (reverse $ take n (reverse s1)) == take n s2 = True
+  | otherwise                                  = False
+
+seq2string :: SeqData -> String
+seq2string = B.unpack . unSD
