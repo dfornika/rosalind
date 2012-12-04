2a106db57a5806181641ee409d2ba5c5031d3050
Dan Fornika <dfornika@gmail.com>
2012-12-04 11:36:35 -0800

    having trouble multiplying Rationals and Integers

--- 15_intro_to_mendelian_inheritance/15_intro_to_mendelian_inheritance.hs
+++ 15_intro_to_mendelian_inheritance/15_intro_to_mendelian_inheritance.hs
@@ -5,12 +5,15 @@ import Data.Ratio
 
 main :: IO ()
 main = do
-  -- It's ugly, but this line converts a string of whitespace-separated
-  -- numbers into a list of integers
+  -- It's ugly, but this line takes a string of whitespace-separated
+  -- numbers from stdin and returns a list of integers
   input <- liftM (map (\x -> read x :: Int)) $ liftM words getLine
+  putStrLn $ show $ fromRational $
+    probSecondMateHomoDom (probFirstMateHomoDom (input!!0) (input!!1) (input!!2)) (input!!0) (input!!1) (input!!2)
+--  putStrLn $ show $ fromRational $
+--    probOffspringPosessDom (input!!0) (input!!1) (input!!2)
 
-  putStrLn $ show $ probOffspringPosessDom (input!!0) (input!!1) (input!!2)
-
+--------------------------------------------------------------------------------
 {-- Consider the following probability table:
 
     firstMate            | A A | A A | A A | A a | A a | A a | a a | a a | a a |
@@ -18,7 +21,7 @@ main = do
     probOffspringHomoDom |  1  | 1/2 |  0  | 1/2 | 1/4 |  0  |  0  |  0  |  0  |
     probOffspringHet     |  0  | 1/2 |  1  | 1/2 | 1/2 | 1/2 |  1  | 1/2 |  0  |
     probOffspringHomoRec |  0  |  0  |  0  |  0  | 1/4 | 1/2 |  0  | 1/2 |  1  |
---}
+
 
 probOffspringPosessDom :: Int -> Int -> Int -> Rational
 probOffspringPosessDom k m n = probOffspringHomoDom + probOffspringHet
@@ -41,21 +44,23 @@ probOffspringPosessDom k m n = probOffspringHomoDom + probOffspringHet
                                (probSecondMateHomoRec (probFirstMateHet k m n) k m n)     * (1%2) +
                                (probSecondMateHomoRec (probFirstMateHomoRec k m n) k m n) * (0%1)
         probOffspringHomoRec = undefined
+--}
 
 probFirstMateHomoDom :: Int -> Int -> Int -> Rational
-probFirstMateHomoDom k m n = (fromIntegral k) % (fromIntegral (k + m + n))
+probFirstMateHomoDom k m n = (toInteger k) % (toInteger $ k + m + n)
 
 probFirstMateHet :: Int -> Int -> Int -> Rational
-probFirstMateHet k m n = (fromIntegral m) % (fromIntegral (k + m + n))
+probFirstMateHet k m n = (toInteger m) % (toInteger $ k + m + n)
 
 probFirstMateHomoRec :: Int -> Int -> Int -> Rational
-probFirstMateHomoRec k m n = (fromIntegral n) % (fromIntegral (k + m + n))
+probFirstMateHomoRec k m n = (toInteger n) % (toInteger $ k + m + n)
 
 probSecondMateHomoDom :: Rational -> Int -> Int-> Int -> Rational
-probSecondMateHomoDom x k m n = undefined
-
+probSecondMateHomoDom x k m n = (x * (k - 1) + (1 - x) * k) % (k + m + n - 1)
+{--
 probSecondMateHet :: Rational -> Int -> Int-> Int -> Rational
-probSecondMateHet x k m n = undefined
+probSecondMateHet x k m n = (x * (m - 1) + (1 - x) * m) % (k + m + n - 1)
 
 probSecondMateHomoRec :: Rational -> Int -> Int-> Int -> Rational
-probSecondMateHomoRec x k m n = undefined
+probSecondMateHomoRec x k m n = (x * (n - 1) + (1 - x) * n) % (k + m + n - 1)
+--}
