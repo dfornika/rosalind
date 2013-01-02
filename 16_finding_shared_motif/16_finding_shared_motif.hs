e7338eca942584cd0f36cab8e8709f6af53853e1
Dan Fornika <dfornika@gmail.com>
2013-01-01 19:34:32 -0800

    a bit of reformatting to fit 80-char lines

--- 16_finding_shared_motif/16_finding_shared_motif.hs
+++ 16_finding_shared_motif/16_finding_shared_motif.hs
@@ -15,11 +15,15 @@ findMotif2 :: String -> String -> String
 findMotif2 [] _ = []
 findMotif2 _ [] = []
 findMotif2 x y = head $ reverse $ sortBy (comparing length)
-                 [matches | matches <- (nub . subsequences) x, (matches `isInfixOf` y && matches `isInfixOf` x)]
+                 [matches
+                 | matches <- (nub . subsequences) x
+                 , (matches `isInfixOf` y && matches `isInfixOf` x)]
 
 -- lcstr is the wikibooks solution:
 -- http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Longest_common_substring#Haskell
 lcstr :: Ord a => [a] -> [a] -> [a]
-lcstr xs ys = maximumBy (compare `on` length) . concat $ [f xs' ys | xs' <- tails xs] ++ [f xs ys' | ys' <- drop 1 $ tails ys]
+lcstr xs ys = maximumBy (compare `on` length)
+              . concat $ [f xs' ys | xs' <- tails xs]
+              ++ [f xs ys' | ys' <- drop 1 $ tails ys]
   where f xs ys = scanl g [] $ zip xs ys
         g z (x, y) = if x == y then z ++ [x] else []
