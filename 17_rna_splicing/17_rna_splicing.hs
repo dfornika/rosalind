6de3101d14033f552c4ea4fca476b9e85ae55c95
Dan Fornika <dfornika@gmail.com>
2013-01-02 20:33:11 -0800

    Making progress on exercise 17 using breakOn function from Data.Text

--- 17_rna_splicing/17_rna_splicing.hs
+++ 17_rna_splicing/17_rna_splicing.hs
@@ -4,7 +4,8 @@ import Control.Monad(liftM)
 import qualified Data.Text as T
 
 main = do
-  (genomicSeq:introns) <- liftM (T.pack . lines) $ getContents
-
-  putStrLn genomicSeq
-  mapM_ putStrLn introns
+  (genomicSeq:introns) <- liftM (T.lines . T.pack) $ getContents
+  let splices = map (flip T.breakOn genomicSeq) introns
+  let donors  = map fst splices
+  let acceptors = zipWith T.stripPrefix (introns) (map snd splices)
+  mapM_ putStrLn $ (map show acceptors)
