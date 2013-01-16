4797917c4a6e1e760d6ee83403eb195d19464de5
Dan Fornika <dfornika@gmail.com>
2013-01-15 21:33:25 -0800

    Implemented fetching fasta data in problem 21.

--- 21_finding_a_protein_motif/21_finding_a_protein_motif.hs
+++ 21_finding_a_protein_motif/21_finding_a_protein_motif.hs
@@ -1,8 +1,17 @@
 module Main where
 
-import Control.Monad(liftM)
+import Control.Monad (liftM, mapM, mapM_)
+import Network.HTTP
 
 main = do
-  uniprotIds <- liftM lines $  getContents
 
-  putStrLn $ show uniprotIds
+  uniprotIds <- liftM lines $  getContents
+  let uniprotStub = "http://www.uniprot.org/uniprot/"
+  fastas <- mapM (\x -> simpleHTTP
+                        (getRequest $ uniprotStub ++ (getId x) ++ ".fasta")
+                        >>= getResponseBody) uniprotIds
+  putStrLn $ show $ uniprotIds
+  mapM_ putStrLn fastas
+
+getId :: String -> String
+getId = takeWhile (/= '_')
