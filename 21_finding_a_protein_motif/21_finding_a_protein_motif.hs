b238b088bd3a921d8387ddaffe68d40ce941414f
Dan Fornika <dfornika@gmail.com>
2013-01-19 13:01:09 -0800

    All output formatting is correct.  Only problem is that I miss overlapping regex matches.  Can't find any documentation on doing overlapping matches.

--- 21_finding_a_protein_motif/21_finding_a_protein_motif.hs
+++ 21_finding_a_protein_motif/21_finding_a_protein_motif.hs
@@ -4,35 +4,34 @@ import Control.Monad (liftM, mapM, mapM_)
 import Data.List (intercalate)
 import Data.List.Split (splitOn)
 import Network.HTTP
+import Text.Regex
 import Text.Regex.Posix
 import qualified Data.ByteString.Lazy.Char8 as B
 import Bio.Core.Sequence (unSD, unSL)
 import Bio.Sequence.Fasta (mkSeqs, seqdata, seqid, Sequence)
 
 main = do
-  uniprotIds <- liftM lines $  getContents
+  accessIDs <- liftM lines $  getContents
 
   let uniprotStub = "http://www.uniprot.org/uniprot/"
   let regex = "N[^P][ST][^P]"
 
   fastas <- mapM (\x -> simpleHTTP
-                        (getRequest $ uniprotStub ++ (getId x) ++ ".fasta")
-                        >>= getResponseBody) uniprotIds
+                        (getRequest $ uniprotStub ++ (getID x) ++ ".fasta")
+                        >>= getResponseBody) accessIDs
 
   let seqs = mkSeqs $ map B.pack $ concatMap lines fastas
 
   let motifResult = map (findMotif regex) seqs
 
-  mapM_ putStrLn $ map formatResult motifResult
+  mapM_ putStr $ map formatResult (zip accessIDs motifResult)
 
-getId :: String -> String
-getId = takeWhile (/= '_')
+getID :: String -> String
+getID = takeWhile (/= '_')
 
-findMotif :: String -> Sequence -> (String, [(MatchOffset, MatchLength)])
-findMotif regex seq = (label, matches)
-  where label = B.unpack $ unSL $ seqid seq
-        matches = getAllMatches ((B.unpack $ unSD $ seqdata seq) =~ regex) :: [(MatchOffset, MatchLength)]
+findMotif :: String -> Sequence -> [(MatchOffset, MatchLength)]
+findMotif regex seq = getAllMatches ((B.unpack $ unSD $ seqdata seq) =~ regex) :: [(MatchOffset, MatchLength)]
 
 formatResult :: (String, [(Int, Int)]) -> String
 formatResult (_, []) = ""
-formatResult (id, matches) = (splitOn "|" id)!!1 ++ "\n" ++ unwords (map (show . (+ 1) . fst) matches)
+formatResult (id, matches) = id ++ "\n" ++ unwords (map (show . (+ 1) . fst) matches) ++ "\n"
