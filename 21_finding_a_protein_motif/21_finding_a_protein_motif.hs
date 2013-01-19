78502cf3454f4b1f43cd4e93739ef6eb047fa5c8
Dan Fornika <dfornika@gmail.com>
2013-01-19 11:31:48 -0800

    Almost completed exercise 21, need to re-format sequence IDs for output, and remove empty lines when no motif is found

--- 21_finding_a_protein_motif/21_finding_a_protein_motif.hs
+++ 21_finding_a_protein_motif/21_finding_a_protein_motif.hs
@@ -1,11 +1,13 @@
 module Main where
 
 import Control.Monad (liftM, mapM, mapM_)
+import Data.List (intercalate)
+import Data.List.Split (splitOn)
 import Network.HTTP
 import Text.Regex.Posix
 import qualified Data.ByteString.Lazy.Char8 as B
-import Bio.Core.Sequence (unSD)
-import Bio.Sequence.Fasta (mkSeqs, seqdata)
+import Bio.Core.Sequence (unSD, unSL)
+import Bio.Sequence.Fasta (mkSeqs, seqdata, seqid, Sequence)
 
 main = do
   uniprotIds <- liftM lines $  getContents
@@ -18,12 +20,19 @@ main = do
                         >>= getResponseBody) uniprotIds
 
   let seqs = mkSeqs $ map B.pack $ concatMap lines fastas
-  let seqString = B.unpack $ unSD $ seqdata (seqs!!0)
-  let output = seqString =~ regex :: Int
-  putStrLn $ show $ uniprotIds
-  mapM_ putStrLn fastas
-  mapM_ putStrLn (map show seqs)
-  putStrLn $ show output
+
+  let motifResult = map (findMotif regex) seqs
+
+  mapM_ putStrLn $ map formatResult motifResult
 
 getId :: String -> String
 getId = takeWhile (/= '_')
+
+findMotif :: String -> Sequence -> (String, [(MatchOffset, MatchLength)])
+findMotif regex seq = (label, matches)
+  where label = B.unpack $ unSL $ seqid seq
+        matches = getAllMatches ((B.unpack $ unSD $ seqdata seq) =~ regex) :: [(MatchOffset, MatchLength)]
+
+formatResult :: (String, [(Int, Int)]) -> String
+formatResult (_, []) = ""
+formatResult (id, matches) = (splitOn "|" id)!!1 ++ "\n" ++ unwords (map (show . (+ 1) . fst) matches)
