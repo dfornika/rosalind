24fafccae50d0f880f6f31e68ac28aa4994e3de6
Dan Fornika <dfornika@gmail.com>
2013-01-28 22:22:49 -0800

    Working on R implementation for exercise 03

--- 03_reverse_complement/R/03_reverse_complement.R
+++ 03_reverse_complement/R/03_reverse_complement.R
@@ -1,4 +1,14 @@
 #!/usr/bin/env Rscript
 
 input <- readLines('stdin', n=1, warn=FALSE)
+rev   <- rev(input)
+output <- rep("N", nchar(input))
 
+for (i in 1:nchar(rev)){
+  if(rev[i]=="A") {output[i]<-"T"}
+  if(rev[i]=="C") output[i]<-"G"    
+  if(rev[i]=="G") output[i]<-"C"    
+  if(rev[i]=="T") output[i]<-"A"
+}
+
+cat(rev(output), '\n')
