fc4d7371ab2f2288c7f8ae9763f91b018bb48db7
Dan Fornika <dfornika@gmail.com>
2013-01-28 22:03:13 -0800

    Finished python implementation for exercise 03

--- 03_reverse_complement/python/03_reverse_complement.py
+++ 03_reverse_complement/python/03_reverse_complement.py
@@ -3,3 +3,12 @@
 import sys
 
 seq = sys.stdin.readline().strip()
+
+def complement(s):
+  """Return the complementary sequence string."""
+  basecomplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+  letters = list(s)
+  letters = [basecomplement[base] for base in letters]
+  return ''.join(letters)
+
+print complement(seq[::-1])
