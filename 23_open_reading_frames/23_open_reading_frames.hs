4fb1f7e1a29c6ba3eb078b2eac2504eacab6d8dc
Dan Fornika <dfornika@gmail.com>
2013-02-16 10:02:18 -0800

    working towards a solution for exercise 23

--- 23_open_reading_frames/23_open_reading_frames.hs
+++ 23_open_reading_frames/23_open_reading_frames.hs
@@ -5,73 +5,95 @@ import qualified Data.Map as Map
 main :: IO()
 main = do
   header <- getLine
-  input  <- getLine
+  fwd    <- getLine
+  let rev = map complement $ reverse fwd
 
-  putStrLn input
+  mapM_ putStrLn $ findOrfs fwd
+  mapM_ putStrLn $ findOrfs (drop 1 fwd)
+  mapM_ putStrLn $ findOrfs (drop 2 fwd)
+  mapM_ putStrLn $ findOrfs rev
+  mapM_ putStrLn $ findOrfs (drop 1 rev)
+  mapM_ putStrLn $ findOrfs (drop 2 rev)
+
+findOrfs :: String -> [String]
+findOrfs seq = splitAt '*' aaString
+  where aaString = dropWhile (/= 'M') $ map translate $ chunk 3 seq
+
+chunk :: Int -> [a] -> [[a]]
+chunk n [] = []
+chunk n xs = ys : chunk n zs
+  where (ys,zs) = splitAt n xs
+
+complement :: Char -> Char
+complement 'A' = 'T'
+complement 'C' = 'G'
+complement 'G' = 'C'
+complement 'T' = 'A'
+complement  _  = 'N'
 
 translate :: String -> Char
 translate key = Map.findWithDefault 'X' key codonMap
-                where codonMap = Map.fromList [ ("UUU", 'F')
-                                              , ("UUC", 'F')
-                                              , ("UUA", 'L')
-                                              , ("UUG", 'L')
-                                              , ("CUU", 'L')
-                                              , ("CUC", 'L')
-                                              , ("CUA", 'L')
-                                              , ("CUG", 'L')
-                                              , ("AUU", 'I')
-                                              , ("AUC", 'I') -- 10
-                                              , ("AUA", 'I')
-                                              , ("AUG", 'M')
-                                              , ("GUU", 'V')
-                                              , ("GUC", 'V')
-                                              , ("GUA", 'V')
-                                              , ("GUG", 'V')
-                                              , ("UCU", 'S')
-                                              , ("UCC", 'S')
-                                              , ("UCA", 'S')
-                                              , ("UCG", 'S') -- 20
-                                              , ("CCU", 'P')
+                where codonMap = Map.fromList [ ("TTT", 'F')
+                                              , ("TTC", 'F')
+                                              , ("TTA", 'L')
+                                              , ("TTG", 'L')
+                                              , ("CTT", 'L')
+                                              , ("CTC", 'L')
+                                              , ("CTA", 'L')
+                                              , ("CTG", 'L')
+                                              , ("ATT", 'I')
+                                              , ("ATC", 'I') -- 10
+                                              , ("ATA", 'I')
+                                              , ("ATG", 'M')
+                                              , ("GTT", 'V')
+                                              , ("GTC", 'V')
+                                              , ("GTA", 'V')
+                                              , ("GTG", 'V')
+                                              , ("TCT", 'S')
+                                              , ("TCC", 'S')
+                                              , ("TCA", 'S')
+                                              , ("TCG", 'S') -- 20
+                                              , ("CCT", 'P')
                                               , ("CCC", 'P')
                                               , ("CCA", 'P')
                                               , ("CCG", 'P')
-                                              , ("ACU", 'T')
+                                              , ("ACT", 'T')
                                               , ("ACC", 'T')
                                               , ("ACA", 'T')
                                               , ("ACG", 'T')
-                                              , ("GCU", 'A')
+                                              , ("GCT", 'A')
                                               , ("GCC", 'A') -- 30
                                               , ("GCA", 'A')
                                               , ("GCG", 'A')
-                                              , ("UAU", 'Y')
-                                              , ("UAC", 'Y')
-                                              , ("UAA", ' ')
-                                              , ("UAG", ' ')
-                                              , ("CAU", 'H')
+                                              , ("TAT", 'Y')
+                                              , ("TAC", 'Y')
+                                              , ("TAA", '*')
+                                              , ("TAG", '*')
+                                              , ("CAT", 'H')
                                               , ("CAC", 'H')
                                               , ("CAA", 'Q')
                                               , ("CAG", 'Q') -- 40
-                                              , ("AAU", 'N')
+                                              , ("AAT", 'N')
                                               , ("AAC", 'N')
                                               , ("AAA", 'K')
                                               , ("AAG", 'K')
-                                              , ("GAU", 'D')
+                                              , ("GAT", 'D')
                                               , ("GAC", 'D')
                                               , ("GAA", 'E')
                                               , ("GAG", 'E')
-                                              , ("UGU", 'C')
-                                              , ("UGC", 'C') -- 50
-                                              , ("UGA", ' ')
-                                              , ("UGG", 'W')
-                                              , ("CGU", 'R')
+                                              , ("TGT", 'C')
+                                              , ("TGC", 'C') -- 50
+                                              , ("TGA", '*')
+                                              , ("TGG", 'W')
+                                              , ("CGT", 'R')
                                               , ("CGC", 'R')
                                               , ("CGA", 'R')
                                               , ("CGG", 'R')
-                                              , ("AGU", 'S')
+                                              , ("AGT", 'S')
                                               , ("AGC", 'S')
                                               , ("AGA", 'R')
                                               , ("AGG", 'R') -- 60
-                                              , ("GGU", 'G')
+                                              , ("GGT", 'G')
                                               , ("GGC", 'G')
                                               , ("GGA", 'G')
                                               , ("GGG", 'G') ]
