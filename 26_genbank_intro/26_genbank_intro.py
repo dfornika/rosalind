cda2b25f36325351d94ace5f33b9fb862b8cfed3
Dan Fornika <dfornika@gmail.com>
2013-02-18 21:18:33 -0800

    completed exercise 26

--- 26_genbank_intro/26_genbank_intro.py
+++ 26_genbank_intro/26_genbank_intro.py
@@ -5,6 +5,6 @@ genus = sys.stdin.readline().rstrip()
 date1 = sys.stdin.readline().rstrip()
 date2 = sys.stdin.readline().rstrip()
 Entrez.email = "dfornika@gmail.com"
-handle = Entrez.esearch(db="nucleotide", term=genus+'[Organism] AND rbcL[Gene] AND ' + date1 + ':' + date2 + '[Publication Date]')
+handle = Entrez.esearch(db="nucleotide", term= genus + '[Organism] AND ' + date1 + ':' + date2 + '[Publication Date]')
 record = Entrez.read(handle)
 print record["Count"]
