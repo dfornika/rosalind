5949d976e1da17b01137bad2b60d2994fcb629ba
Dan Fornika <dfornika@gmail.com>
2013-02-18 21:54:12 -0800

    solved exercise 27 pubmed searching

--- 27_pubmed_searching/27_pubmed_searching.py
+++ 27_pubmed_searching/27_pubmed_searching.py
@@ -2,9 +2,9 @@ import sys
 from Bio import Entrez
 
 author = sys.stdin.readline().rstrip()
-date   = sys.stdin.readline().rstrip()
+year   = sys.stdin.readline().rstrip()
 Entrez.email = "dfornika@gmail.com"
-handle = Entrez.esearch(db="pubmed", term=author + '[Author] AND ' + date + '[dp]')
+handle = Entrez.esearch(db="pubmed", term=author + '[Author] AND ' + year + '[Publication Date]', retmax='100')
 record = Entrez.read(handle)
 pmids = map(int, record["IdList"])
 print sorted(pmids)[0]
