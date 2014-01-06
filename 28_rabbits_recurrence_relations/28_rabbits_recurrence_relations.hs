ffd0fc8bbf947aaca7246a2641b458b39a5854a3
Dan Fornika <dfornika@gmail.com>
2014-01-05 17:23:14 -0800

    Naive solution for exercise 28, very slow for n > 30

--- 28_rabbits_recurrence_relations/28_rabbits_recurrence_relations.hs
+++ 28_rabbits_recurrence_relations/28_rabbits_recurrence_relations.hs
@@ -4,10 +4,12 @@ import Control.Monad(liftM)
 
 main :: IO()
 main = do
-  [n,k] <- liftM (map (\x -> read x :: Int)) $ liftM words getLine
+  [n,k] <- liftM (map (\x -> read x :: Integer)) $ liftM words getLine
   putStrLn $ show $ rabbitPairs n k
 
-rabbitPairs :: Int -> Int -> Int
+rabbitPairs :: Integer -> Integer -> Integer
 rabbitPairs 1 _ = 1
 rabbitPairs 2 _ = 1
-rabbitPairs n k = rabbitPairs (n-1) k
+rabbitPairs n k = (maturePairs n k) + (immaturePairs n k)
+  where immaturePairs  n k = k * (maturePairs (n - 1) k)
+        maturePairs    n k = rabbitPairs (n - 1) k
