afb8dbe1e51820e4ff0a602529bb93b174ef3c16
Dan Fornika <dfornika@gmail.com>
2014-01-05 19:43:46 -0800

    Updated to quasi-stateful implementation, faster for large n

--- 28_rabbits_recurrence_relations/28_rabbits_recurrence_relations.hs
+++ 28_rabbits_recurrence_relations/28_rabbits_recurrence_relations.hs
@@ -1,15 +1,51 @@
 module Main where
 
-import Control.Monad(liftM)
+import Control.Monad (liftM)
+import Control.Monad.State
+import Data.List (genericTake)
+
+data Rabbits = Rabbits { mature   :: Integer
+                       , immature :: Integer
+                       , newborns :: Integer
+                       }
 
 main :: IO()
 main = do
   [n,k] <- liftM (map (\x -> read x :: Integer)) $ liftM words getLine
   putStrLn $ show $ rabbitPairs n k
 
+initialState :: Rabbits
+initialState = Rabbits { mature   = 0
+                       , immature = 1
+                       , newborns = 0
+                       }
+
 rabbitPairs :: Integer -> Integer -> Integer
 rabbitPairs 1 _ = 1
 rabbitPairs 2 _ = 1
-rabbitPairs n k = (maturePairs n k) + (immaturePairs n k)
-  where immaturePairs  n k = k * (maturePairs (n - 1) k)
-        maturePairs    n k = rabbitPairs (n - 1) k
+rabbitPairs n k = total $ last $ genericTake n $ iterate (tick k) initialState
+
+becomeMature :: Rabbits -> Rabbits
+becomeMature r = Rabbits { mature   = (mature r + immature r)
+                         , immature = 0
+                         , newborns = newborns r
+                         }
+
+breed :: Integer -> Rabbits -> Rabbits
+breed k r = Rabbits { mature   = mature r
+                    , immature = immature r
+                    , newborns = new
+                    }
+  where new = mature r * k
+
+clearNewborns :: Rabbits -> Rabbits
+clearNewborns r = Rabbits { mature = mature r
+                          , immature = immature r + newborns r
+                          , newborns = 0
+                          }
+
+total :: Rabbits -> Integer
+total r = mature r + immature r
+
+tick :: Integer -> Rabbits -> Rabbits
+tick k r = clearNewborns $ becomeMature $ breed k r
