1aae5ab734b890dceee5ecbccb5520544f119754
Dan Fornika <dfornika@gmail.com>
2015-08-15 07:14:06 -0700

    Added solution to exercise 01 in C

---
+++ 01_counting_nucleotides/c/01_counting_nucleotides.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct BaseCounts {
+  int a;
+  int c;
+  int g;
+  int t;
+};
+
+int MAX_STRING_LEN = 1000;
+
+int main (int argc, char *argv) {
+  char s[MAX_STRING_LEN];
+  struct BaseCounts counts = {0, 0, 0, 0};
+
+  fgets(s, MAX_STRING_LEN, stdin);
+
+  int i = 0;
+  do {
+    switch(s[i]) {
+      case 'A':
+        counts.a += 1;
+        break;
+      case 'C':
+        counts.c += 1;
+        break;
+      case 'G':
+        counts.g += 1;
+        break;
+      case 'T':
+        counts.t += 1;
+        break;
+      default:
+        break;
+    }
+    i++;
+  } while (s[i] != '\n');
+
+  printf("%d %d %d %d\n", counts.a, counts.c, counts.g, counts.t);
+}
